04131a7a51b143ccfa1db7d81816f53799101aa2
johanuribe03 <johan.uribe@gmail.com>
2020-07-13 10:58:01 +0000

    update 7/13/20

--- exercises/05.1-Loop-from-one-to-seventeen/app.js
+++ exercises/05.1-Loop-from-one-to-seventeen/app.js
@@ -1,1 +1,11 @@
 //you code here
+
+numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
+
+function loop1to17(arr) {
+    for (i=0; i< arr.length; i++){
+        console.log(arr[i]);
+    }
+}
+
+loop1to17(numbers);
--- exercises/05.2-Loop-from-seven-to-twelve/app.js
+++ exercises/05.2-Loop-from-seven-to-twelve/app.js
@@ -1,1 +1,10 @@
 //you code here
+numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
+
+function loop1to17(arr) {
+    for (i=6; i< arr.length-5; i++){
+        console.log(arr[i]);
+    }
+}
+
+loop1to17(numbers);
