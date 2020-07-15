710588f703cb9c2ef7ebdf841946795d338aa316
johanuribe03 <johan.uribe@gmail.com>
2020-07-15 10:40:59 +0000

    update 7/15/20

--- .breathecode/resets/07.2-Letter-Counter/app.js
+++ .breathecode/resets/07.2-Letter-Counter/app.js
@@ -1,12 +1,6 @@
 let par = "Lorem ipsum dolor sit amet consectetur adipiscing elit Curabitur eget bibendum turpis Curabitur scelerisque eros ultricies venenatis mi at tempor nisl Integer tincidunt accumsan cursus"
-
-
 let counts = {};
+
 // your code here
 
-removedSpaces = par.split(" ").join(""); //remove spaces
-par2 = removedSpaces.toLowerCase(); //toLowerCase
-
-obj = JSON.parse(par2);
-console.log(par2);
-count = 1;
+console.log(counts);
--- exercises/08.1-Mixed-array/app.js
+++ exercises/08.1-Mixed-array/app.js
@@ -1,3 +1,12 @@
 var mix = [42, true, "towel", [2,1], 'hello', 34.4, {"name": "juan"}];
 
 //your code here
+
+var mix2 = [];
+
+for(let i=0; i<mix.length; i++){
+  mix2.push(mix[i].typeOf());  
+}
+
+console.log(mix2);
+
