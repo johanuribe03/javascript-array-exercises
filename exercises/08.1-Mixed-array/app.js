823b013561254822248fbd3b320e3b275d193d84
johanuribe03 <johan.uribe@gmail.com>
2020-07-15 10:48:31 +0000

    update 7/15/20 2

--- exercises/08.1-Mixed-array/app.js
+++ exercises/08.1-Mixed-array/app.js
@@ -4,8 +4,9 @@ var mix = [42, true, "towel", [2,1], 'hello', 34.4, {"name": "juan"}];
 
 var mix2 = [];
 
-for(let i=0; i<mix.length; i++){
-  mix2.push(mix[i].typeOf());  
+for(let i=0; i < mix.length; i++){
+  mix2.push(typeof mix[i]);
+  // console.log()
 }
 
 console.log(mix2);
